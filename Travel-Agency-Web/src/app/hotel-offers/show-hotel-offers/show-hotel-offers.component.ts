dec76dba25dc9755afffe5f015e03e6464cdb661
Daniel <danielabad@gmail.com>
2023-11-11 10:31:36 -0500

    Mostrar sin filtro por defecto

--- Travel-Agency-Web/src/app/hotel-offers/show-hotel-offers/show-hotel-offers.component.ts
+++ Travel-Agency-Web/src/app/hotel-offers/show-hotel-offers/show-hotel-offers.component.ts
@@ -3,6 +3,7 @@ import { SharedService } from 'src/app/shared.service';
 import { Offer } from '../../models/offer';
 import { HotelFilter } from 'src/app/models/filterMapper';
 import { HttpParams } from '@angular/common/http';
+import { filter } from 'rxjs';
 
 
 @Component({
@@ -12,11 +13,25 @@ import { HttpParams } from '@angular/common/http';
 })
 export class ShowHotelOffersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SharedService) { }
   showFilter: boolean = false;
   HotelOffersList: Offer[] = [];
 
   ngOnInit(): void {
+    let filter: HotelFilter;
+
+// Me creo un filtro vacio  
+        filter = {
+          productId: null,
+          startDate: null,
+          startPrice: null,
+          endPrice: null,
+          agencyId: null,
+          hotelName: null
+        };
+        this.service.getHotelOffers(filter).subscribe(data => {
+          this.HotelOffersList = data;
+        });
   }
   onFilter(data: any[]) {
     // Asigna los resultados del filtro a la variable
