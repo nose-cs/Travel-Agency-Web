502319c3e78e5c7722465c4ced5cadb58e6bdc61
Enzo D'toste <enzord2001@gmail.com>
2023-11-20 18:59:14 -0500

    edit-remove-offers

---
+++ Travel-Agency-Web/src/app/hotel-offers/show-staff-hotel-offers/show-staff-hotel-offers.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShowStaffHotelOffersComponent } from './show-staff-hotel-offers.component';
+
+describe('ShowStaffHotelOffersComponent', () => {
+  let component: ShowStaffHotelOffersComponent;
+  let fixture: ComponentFixture<ShowStaffHotelOffersComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ShowStaffHotelOffersComponent]
+    });
+    fixture = TestBed.createComponent(ShowStaffHotelOffersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- Travel-Agency-Web/src/app/staff-site/marketing/marketing.component.ts
+++ Travel-Agency-Web/src/app/staff-site/marketing/marketing.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 import { Offer } from '../../models/offer';
 import { Router } from '@angular/router';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
-import { CreateHotelOffersComponent } from '../../hotel-offers/create-hotel-offers/create-hotel-offers.component';
+import { CreateEditOffersComponent } from '../../create-edit-offers/create-edit-offers.component';
+import { ShowStaffHotelOffersComponent } from '../../hotel-offers/show-staff-hotel-offers/show-staff-hotel-offers.component';
+import { SharedService } from '../../shared.service';
+import { HotelFilter } from '../../models/hotelFilter';
+import { Hotel } from '../../models/hotel';
 
 @Component({
   selector: 'app-marketing',
@@ -12,7 +16,7 @@ import { CreateHotelOffersComponent } from '../../hotel-offers/create-hotel-offe
 })
 export class MarketingComponent {
 
-  constructor(private router: Router, private dialogService: DialogService) { }
+  constructor(private service: SharedService, private router: Router, private dialogService: DialogService) { }
 
   OffersControlItems: string[] = ["Hotel Offers", "Flight Offers", "Tour Offers", "Packages"];
 
@@ -25,11 +29,41 @@ export class MarketingComponent {
 
         switch (action) {
           case "Create":
-            this.ref = this.dialogService.open(CreateHotelOffersComponent, {
+            const offer = new Offer();
+
+            this.ref = this.dialogService.open(CreateEditOffersComponent, {
+              data: {
+                offer: offer,
+                execute: (hotelOffer: Offer) => this.service.createHotelOffer(hotelOffer),
+                filter: (query: string) => {
+                  const filter = new HotelFilter();
+                  filter.hotelName = query;
+
+                  let suggestions: { id: number, name: string }[] = [];
+                  this.service.getHotelsWithFilter(filter).subscribe(
+                    (hotels: Hotel[]) => {
+                      for (let sugg of hotels.map(hotel => { return { id: hotel.id, name: hotel.name }; })) {
+                        suggestions.push(sugg);
+                      }
+                    }
+                  );
+
+                  return suggestions;
+                }
+              },
               header: 'Create a Hotel offer',
               contentStyle: { overflow: 'auto' },
               baseZIndex: 10000,
               maximizable: false
+            });
+            break;
+
+          case "Manage":
+            this.ref = this.dialogService.open(ShowStaffHotelOffersComponent, {
+              contentStyle: { overflow: 'auto' },
+              baseZIndex: 10000,
+              width: '70%',
+              maximizable: true
             });
             break;
         }
